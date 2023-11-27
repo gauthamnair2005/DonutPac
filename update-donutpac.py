2ab59d904940d88cc509e50d378d7a26a2389e8b
Gautham Nair <gautham.nair.2005@outlook.com>
2023-11-27 17:02:23 +0530

    Update update-donutpac.py

--- update-donutpac.py
+++ update-donutpac.py
@@ -1,11 +1,11 @@
 import os
-import sys
 
 print("DonutPac Updater ")
 print("-"*20)
 print()
-print("Connecting to the Server...")
-print("Loading Data...")
-os.system('wget https://raw.githubusercontent.com/gauthamnair2005/DonutPac/main/donutpac.py -O donutpac.py')
-print("Verifying Download..")
-print("DonutPac Updated successfully..")
+print("Obtaining the Latest Version of DonutPAC")
+try:
+  os.system('wget https://raw.githubusercontent.com/gauthamnair2005/DonutPac/main/donutpac.py -O donutpac.py')
+  print("DonutPac Updated successfully...")
+except:
+  print("An error ocurred")
