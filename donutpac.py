365d77bea96cd6205136143708e1f93e9134a220
Gautham Nair <gautham.nair.2005@outlook.com>
2023-11-30 19:31:23 +0530

    Update donutpac.py

--- donutpac.py
+++ donutpac.py
@@ -46,6 +46,8 @@ def install_package(package, packages):
                     print(f"Extracting {package}.tar.gz")
                     subprocess.run(['tar', '-xf', f'{package}.tar.gz'])
 
+                    subprocess.run(['mv',f"{package}-{packages[package]['version']}",f"{package}")
+
                     print(f"Configuring {package}")
                     subprocess.run(['./configure'], cwd=package, check=True)
 
