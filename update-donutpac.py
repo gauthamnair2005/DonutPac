71f3dd4b08fad2e2a3d3137cb4b269821556cb9a
Gautham Nair <gautham.nair.2005@outlook.com>
2023-12-01 21:42:23 +0530

    modified:   update-donutpac.py

--- update-donutpac.py
+++ update-donutpac.py
@@ -1,11 +1,15 @@
-import os
+import requests
 
 print("DonutPac Updater ")
 print("-"*20)
 print()
 print("Obtaining the Latest Version of DonutPAC")
+
 try:
-  os.system('wget https://raw.githubusercontent.com/gauthamnair2005/DonutPac/main/donutpac.py -O donutpac.py')
-  print("DonutPac Updated successfully...")
-except:
-  print("An error ocurred")
+    response = requests.get('https://raw.githubusercontent.com/gauthamnair2005/DonutPac/main/donutpac.py')
+    response.raise_for_status()  # Raise an exception if the request was unsuccessful
+    with open('donutpac.py', 'w') as file:
+        file.write(response.text)
+    print("DonutPac Updated successfully...")
+except requests.RequestException as e:
+    print(f"An error occurred: {e}")
